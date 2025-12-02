906601c31141edd31fa1dfb99617fb5fc3c80a7f
Jai Bellare <jaibellare@gmail.com>
2025-12-02 22:57:34 +0530

    add Navbar with logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
   import './global.css'
 
+  import Navbar from './Navbar.svelte';
   let { children } = $props();
 </script>
 
+<Navbar />
 
 {@render children()}
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,72 @@
+<script>
+  // https://www.okupter.com/blog/deploy-sveltekit-website-to-github-pages
+  import { base } from '$app/paths';
+  import logo from '$lib/ssa-iitb-logo.jpg';
+
+  const links = [
+    ['', 'Home'],
+    ['contact', 'Contact']
+  ];
+</script>
+
+<nav>
+  <img src={logo} id="logo" alt="SSA IITB Logo" />
+
+  <div id="links">
+    {#each links as link}
+      <a href="{base}/{link[0]}">{link[1]}</a>
+    {/each}
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    background-color: var(--bg);
+
+    padding: 0 4rem;
+    height: var(--navbar-height);
+    justify-content: center;
+    align-items: center;
+
+    border-bottom: 1.5px solid var(--fg-gray);
+  }
+
+  img {
+    height: 100%;
+  }
+
+  #logo_white {
+    display: none;
+  }
+
+  :global(body.dark) {
+    #logo_black {
+      display: none;
+    }
+
+    #logo_white {
+      display: block;
+    }
+  }
+
+  #links {
+    display: flex;
+    padding: 0.9rem 4rem;
+    font-size: 1.4rem;
+    gap: 3rem;
+    justify-content: center;
+    align-items: center;
+  }
+
+  a {
+    color: var(--fg);
+  }
+
+  @media (max-aspect-ratio: 1) {
+    #links {
+      display: none;
+      /* TODO */
+    }
+  }
+</style>
