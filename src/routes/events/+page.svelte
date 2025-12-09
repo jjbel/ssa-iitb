d5fde59bc8a5f7277f6e0e60e52ec1531df76041
Jai Bellare <jaibellare@gmail.com>
2025-12-09 22:19:31 +0530

    add events subpage

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,7 +1,31 @@
-<div>
+<script>
+  import Event from './Event.svelte';
+  import events from './events.json';
+</script>
+
+<div id="events">
   <h1>Past Events</h1>
+
+  {#each events as event}
+    <Event
+      date={event.date}
+      title={event.title}
+      desc={event.desc}
+      imgs={event.imgs}
+    />
+  {/each}
 </div>
 
 <style>
-    
+  #events {
+    padding: 0 10% 0 10%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+  }
+
+  h1 {
+    margin-bottom: -2rem;
+  }
 </style>
---
+++ src/routes/events/Event.svelte
@@ -0,0 +1,38 @@
+<!-- https://stackoverflow.com/questions/78664060/how-do-we-do-named-slots-in-in-svelte-5 -->
+
+<script>
+  let { date, title, desc, imgs } = $props();
+  import { base } from '$app/paths';
+</script>
+
+<div class="event">
+  <h2>{title}</h2>
+  <h3>{date}</h3>
+  <p>{desc}</p>
+
+  <ul class="gallery">
+    {#each imgs as img}
+      <img src={`${base}/events/${date}/${img}`} alt="" />
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .gallery {
+    width: 100%;
+
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  img {
+    height: 15rem;
+    width: auto;
+  }
+
+  p {
+    color: rgb(25, 25, 25);
+    margin: 0.8rem 0 0.8rem 0;
+  }
+</style>
